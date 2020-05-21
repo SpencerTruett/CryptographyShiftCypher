1dc1e9f58508575ba06350826987e87a89f96d6a
Spencer Truett <srtruett1993@gmail.com>
2020-05-20 19:04:48 -0500

    added while loop

--- CryptographyShiftCypher.py
+++ CryptographyShiftCypher.py
@@ -1,49 +1,60 @@
-answer = input("Would you like to encrypt or decrypt?: ")
 
-if answer == "encrypt":
-    letters = input("Please enter a word to encrypt: ").lower()
-    shiftNumber = input("Please enter a number to use as a key: ")
+while True:
+    answer = input("Would you like to encrypt or decrypt?: ")
 
-
-    numbers = []
-
-    for letter in letters:
-
-      number = ord(letter) - 96 + int(shiftNumber)
-
-      numbers.append(number)
-
-    newLetters = []
-
-    for number in numbers:
-
-        newLetter = chr(ord('`')+number)
-
-        newLetters.append(newLetter)
-
-    separator = ''
-    print("Your encrypted word is: " + separator.join(newLetters))
-
-if answer == "decrypt":
-    letters = input("Please enter a word to decrypt: ").lower()
-    shiftNumber = input("Please enter a number to use as a key: ")
+    if answer == "encrypt":
+        letters = input("Please enter a word to encrypt: ").lower()
+        shiftNumber = input("Please enter a number to use as a key: ")
 
 
-    numbers = []
+        numbers = []
 
-    for letter in letters:
+        for letter in letters:
 
-      number = ord(letter) - 96 - int(shiftNumber)
+          number = ord(letter) - 96 + int(shiftNumber)
 
-      numbers.append(number)
+          numbers.append(number)
 
-    newLetters = []
+        newLetters = []
 
-    for number in numbers:
+        for number in numbers:
 
-        newLetter = chr(ord('`')+number)
+            newLetter = chr(ord('`')+number)
 
-        newLetters.append(newLetter)
+            newLetters.append(newLetter)
 
-    separator = ''
-    print("Your decrypted word is: " + separator.join(newLetters))
+        separator = ''
+        print("Your encrypted word is: " + separator.join(newLetters))
+
+    if answer == "decrypt":
+        letters = input("Please enter a word to decrypt: ").lower()
+        shiftNumber = input("Please enter a number to use as a key: ")
+
+
+        numbers = []
+
+        for letter in letters:
+
+          number = ord(letter) - 96 - int(shiftNumber)
+
+          numbers.append(number)
+
+        newLetters = []
+
+        for number in numbers:
+
+            newLetter = chr(ord('`')+number)
+
+            newLetters.append(newLetter)
+
+        separator = ''
+        print("Your decrypted word is: " + separator.join(newLetters))
+
+    loopAgain = input("Would you like to go again? (Yes/No): ").lower()
+
+    if loopAgain == "yes":
+        continue
+
+    elif loopAgain == "no":
+        print("Goodbye")
+        break
